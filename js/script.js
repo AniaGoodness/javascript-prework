2ff19577d209badc0fe358ef416d6c90d79d7052
Anna Grabowska <aniagoodness@gmail.com>
2020-01-27 21:00:13 +0100

    score

--- js/script.js
+++ js/script.js
@@ -6,19 +6,19 @@ let computerMove = 'nieznany ruch';
 
 if(randomNumber == 1){
   computerMove = 'kamień';
-}
 printMessage('Mój ruch to: ' + computerMove);
-
+}
 else if(randomNumber == 2){
   computerMove = 'papier';
-}
 printMessage('Mój ruch to: ' + computerMove);
-
+}
 else if(randomNumber == 3){
   computerMove = 'nożyce';
 
 }
 printMessage('Mój ruch to: ' + computerMove);
+
+//ruch gracza
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
@@ -28,16 +28,40 @@ let playerMove = 'nieznany ruch';
 
 if(playerInput == '1'){
   playerMove = 'kamień';
-}
 printMessage('Twój ruch to: ' + playerMove);
-
+}
 else if(playerInput == '2'){
   playerMove = 'papier';
-
-}
 printMessage('Twój ruch to: ' + playerMove);
-
+}
 else if(playerInput == '3'){
   playerMove = 'nożyce';
 }
 printMessage('Twój ruch to: ' + playerMove);
+
+//wynik
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+}
+else if(computerMove == playerMove ){
+  printMessage('Remis!');
+}
+else if(computerMove == 'kamień' && playerMove == 'nożyce'){
+  printMessage('Ty przegrywasz!');
+}
+else if(computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz!');
+}
+else if ( computerMove == 'papier' && playerMove == 'kamień'){
+  printMessage('Ty przegrywasz!');
+}
+else if ( computerMove == 'nożyce' && playerMove == 'papier'){
+  printMessage('Ty przegrywasz!');
+}
+else if(computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+}
+else {
+  printMessage('Możesz wybrać tylko 1, 2 lub 3')
+}
