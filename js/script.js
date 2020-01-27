4bf07d794774462ff135be936043a7bb034d187a
Anna Grabowska <aniagoodness@gmail.com>
2020-01-27 20:29:21 +0100

    playermove123

--- js/script.js
+++ js/script.js
@@ -7,7 +7,17 @@ let computerMove = 'nieznany ruch';
 if(randomNumber == 1){
   computerMove = 'kamień';
 }
+printMessage('Mój ruch to: ' + computerMove);
 
+else if(randomNumber == 2){
+  computerMove = 'papier';
+}
+printMessage('Mój ruch to: ' + computerMove);
+
+else if(randomNumber == 3){
+  computerMove = 'nożyce';
+
+}
 printMessage('Mój ruch to: ' + computerMove);
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
@@ -19,5 +29,15 @@ let playerMove = 'nieznany ruch';
 if(playerInput == '1'){
   playerMove = 'kamień';
 }
+printMessage('Twój ruch to: ' + playerMove);
 
+else if(playerInput == '2'){
+  playerMove = 'papier';
+
+}
+printMessage('Twój ruch to: ' + playerMove);
+
+else if(playerInput == '3'){
+  playerMove = 'nożyce';
+}
 printMessage('Twój ruch to: ' + playerMove);
