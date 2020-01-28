8c989b7ceb5693940bf2bf178185d2c6c53026f0
Anna Grabowska <aniagoodness@gmail.com>
2020-01-28 23:17:54 +0100

    function

--- js/script.js
+++ js/script.js
@@ -2,9 +2,23 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+function getMoveName(randomNumber, playerInput){
+  if(randomNumber == 1){
+    return 'kamień';
+  } else if(randomNumber == 2){
+    return 'papier';
+  } else if(randomNumber == 3){
+    return 'nożyce';
+  } else {
+    printMessage('Nie znam ruchu o id ' + randomNumber + '.');
+    return 'nieznany ruch';
+  }
+}
 
-if(randomNumber == 1){
+let argComputerMove = getMoveName(randomNumber);
+
+
+/* if(randomNumber == 1){
   computerMove = 'kamień';
 }
 else if(randomNumber == 2){
@@ -13,30 +27,30 @@ else if(randomNumber == 2){
 else if(randomNumber == 3){
   computerMove = 'nożyce';
 
-}
-printMessage('Mój ruch to: ' + computerMove);
+} */
+//printMessage('Mój ruch to: ' + argComputerMove);
 
-//ruch gracza
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
+let argPlayerMove = getMoveName(playerInput);
 
-let playerMove = 'nieznany ruch';
-
-if(playerInput == '1'){
-  playerMove = 'kamień';
+ /* if(playerInput == '1'){
+  argPlayerMove = 'kamień';
 }
 else if(playerInput == '2'){
-  playerMove = 'papier';
+  argPlayerMove = 'papier';
 }
 else if(playerInput == '3'){
-  playerMove = 'nożyce';
+  argPlayerMove = 'nożyce';
 }
-printMessage('Twój ruch to: ' + playerMove);
+*/
+
+//printMessage('Twój ruch to: ' + argPlayerMove);
 
 //wynik
-
+/*
 if( computerMove == 'kamień' && playerMove == 'papier'){
   printMessage('Ty wygrywasz!');
 }
@@ -61,3 +75,35 @@ else if(computerMove == 'nożyce' && playerMove == 'kamień'){
 else {
   printMessage('Możesz wybrać tylko 1, 2 lub 3')
 }
+*/
+console.log('moves:', argComputerMove, argPlayerMove);
+
+function displayResult(argComputerMove, argPlayerMove){
+    printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+
+    if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+      printMessage('Ty wygrywasz!');
+    }
+      else if(argComputerMove == argPlayerMove ){
+        printMessage('Remis!');
+      }
+      else if(argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
+        printMessage('Ty przegrywasz!');
+      }
+      else if(argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
+        printMessage('Ty wygrywasz!');
+      }
+      else if ( argComputerMove == 'papier' && argPlayerMove == 'kamień'){
+        printMessage('Ty przegrywasz!');
+      }
+      else if ( argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
+        printMessage('Ty przegrywasz!');
+      }
+      else if(argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
+        printMessage('Ty wygrywasz!');
+      }
+      else {
+        printMessage('Możesz wybrać tylko 1, 2 lub 3')
+      }
+    }
+let result1 = displayResult(argComputerMove, argPlayerMove);
