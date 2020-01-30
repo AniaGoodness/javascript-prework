1333ce13a08fa3c2b1f31996233ef27ea12e706a
Anna Grabowska <aniagoodness@gmail.com>
2020-01-30 21:23:12 +0100

    buttonsend

--- js/script.js
+++ js/script.js
@@ -1,6 +1,42 @@
-function playGame(){
+{
+  const rock = document.getElementById('playRock');
+	const	paper = document.getElementById('playPaper');
+	const	scissors = document.getElementById('playScissors');
 
-  function buttonClicked(playerInput){
+  function printMessage(msg){
+  	var div = document.createElement('div');
+  	div.innerHTML = msg;
+  	document.getElementById('messages').appendChild(div);
+  }
+
+  function clearMessages(){
+  	document.getElementById('messages').innerHTML = '';
+  }
+
+  function playGame(playerInput){
+    clearMessages()
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    let argPlayerMove = getMoveName(playerInput);
+    let argComputerMove = getMoveName(randomNumber);
+    displayResult(argPlayerMove, argComputerMove);
+  }
+
+  function getMoveName(argMoveId){
+    if(argMoveId == 1){
+      return 'kamień';
+    } else if(argMoveId == 2){
+      return 'papier';
+    } else if(argMoveId == 3){
+      return 'nożyce';
+    } else {
+      printMessage('Nie znam ruchu o id ' + randomNumber + '.');
+      return 'nieznany ruch';
+      }
+  }
+
+
+
+  /*function buttonClicked(playerInput){
     printMessage('Dokonałeś wyboru');
     let playerInput;
 
@@ -12,39 +48,14 @@ function playGame(){
       playGame('nożyce')
     }
   }
-
     playerInput.addEventListener('click', playGame);
-
-  /*let argPlayerMove = getMoveName(playerInput);
+    let argPlayerMove = getMoveName(playerInput);
     printMessage('Wybrałeś' + playerInput)
-
-  console.log('Gracz wpisał: ' + playerInput);*/
-
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
-
+  console.log('Gracz wpisał: ' + playerInput);
   console.log('Wylosowana liczba to: ' + randomNumber);
-
-  function getMoveName(randomNumber, playerInput){
-    if(randomNumber == 1){
-      return 'kamień';
-    } else if(randomNumber == 2){
-      return 'papier';
-    } else if(randomNumber == 3){
-      return 'nożyce';
-    } else {
-      printMessage('Nie znam ruchu o id ' + randomNumber + '.');
-      return 'nieznany ruch';
-      }
-  }
-
   let argComputerMove = getMoveName(randomNumber);
-
-  /* let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');*/
-
-
-
-
-  console.log('moves:', argComputerMove, argPlayerMove);
+  let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+  console.log('moves:', argComputerMove, argPlayerMove); */
 
   function displayResult(argComputerMove, argPlayerMove){
     printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
@@ -74,11 +85,13 @@ function playGame(){
         printMessage('Możesz wybrać tylko 1, 2 lub 3')
       }
     }
-    let result1 = displayResult(argComputerMove, argPlayerMove);
-
-    let buttons = document.getElementById(playRock, playPaper, playScissors);
-    playRock.addEventListener('click', Kamień);
-    playPaper.addEventListener('click', Papier);
-    playScissors.addEventListener('click', Nożyce);
+    rock.addEventListener('click',function(){
+    playGame(1);
+		});
+	paper.addEventListener('click',function(){
+		playGame(2);
+		});
+	scissors.addEventListener('click',function(){
+		playGame(3);
+		});
 }
-playGame()
